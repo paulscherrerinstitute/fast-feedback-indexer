5f2b44395e2502c2f800819033db67f822552425
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2022-08-30 10:26:12 +0200

    Add archiving script

---
+++ archive.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Create a tar.xz archive of the code in the parent directory.
+# This script only works if executed from the top directory of the repository.
+#
+# Usage: sh archive.sh
+# Result: ../fast-feedback-indexer.tar.xz
+
+if test -r archive.sh; then
+    tar cvJf ../fast-feedback-indexer.tar.xz --exclude=build *
+else
+    echo "Failed: execute this command from the top directory of the repository!"
+fi
