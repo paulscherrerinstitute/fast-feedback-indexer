87ec406bfd86697d43fbb0634db542729eb9a4b3
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2022-11-25 14:41:01 +0100

    Add lsq refinement indexer - with interface suggested by Filip

---
+++ indexer/src/refine.h
@@ -0,0 +1,283 @@
+/*
+Copyright 2022 Paul Scherrer Institute
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+
+------------------------
+
+Author: hans-christian.stadler@psi.ch
+*/
+
+#ifndef INDEXER_REFINE_H
+#define INDEXER_REFINE_H
+
+#include <Eigen/Dense>
+#include <Eigen/LU>
+#include <exception.h>
+#include <numeric>
+#include <functional>
+#include "indexer.h"
+
+namespace fast_feedback {
+    namespace refine {
+
+        // base indexer class for refinement
+        // - controlling all indexer data
+        // - getter/setter interface
+        template<typename float_type=float>
+        class indexer {
+          protected:
+            Eigen::Matrix<float_type, Eigen::Dynamic, 3u> coords;  // for input cells + spots in reciprocal space
+            Eigen::Matrix<float_type, Eigen::Dynamic, 3u> cells;   // output cells coordinate container
+            Eigen::Vector<float_type, Eigen::Dynamic> scores;      // output cell scores container
+            fast_feedback::config_runtime<float_type> crt;
+            fast_feedback::indexer<float_type> idx;
+            fast_feedback::memory_pin pin_coords;                   // pin input coordinate container
+            fast_feedback::memory_pin pin_cells;                    // pin output cells coordinate container
+            fast_feedback::memory_pin pin_scores;                   // pin output cell scores container
+            fast_feedback::memory_pin pin_crt;                      // pin runtime config memory
+          public:
+            inline static void check_config (const fast_feedback::config_persistent<float_type>& cp,
+                                             const fast_feedback::config_runtime<float_type>& cr)
+            {
+                if (cp.max_input_cells <= 0u)
+                    throw FF_EXCEPTION("no input cells");
+                if (cp.max_output_cells <= 0)
+                    throw FF_EXCEPTION("no output cells");
+                if (cp.max_spots <= 0u)
+                    throw FF_EXCEPTION("no spots");
+                if (cp.num_candidate_vectors < 1u)
+                    throw FF_EXCEPTION("nonpositive number of candidate vectors");
+                if (cr.num_sample_points < cp.num_candidate_vectors)
+                    throw FF_EXCEPTION("fewer sample points than required candidate vectors");
+            }
+
+            inline indexer (const fast_feedback::config_persistent<float_type>& cp,
+                            const fast_feedback::config_runtime<float_type>& cr)
+                : coords{cp.max_spots + 3u * cp.max_input_cells, 3u},
+                  cells{3u * cp.max_output_cells, 3u}, scores{cp.max_output_cells},
+                  crt{cr}, idx{cp},
+                  pin_coords{coords}, pin_cells{cells}, pin_scores{scores},
+                  pin_crt{fast_feedback::memory_pin::on(cr)}
+            {
+                check_config(cp, cr);
+            }
+
+            inline indexer (indexer&&) = default;
+            inline indexer& operator= (indexer&&) = default;
+            inline virtual ~indexer () = default;
+
+            indexer () = delete;
+            indexer (const indexer&) = delete;
+            indexer& operator= (const indexer&) = delete;
+
+            inline virtual void index (unsigned n_input_cells, unsigned n_spots)
+            {
+                fast_feedback::input<float_type> input{&coords(0,0), &coords(0,1), &coords(0,2), n_input_cells, n_spots};
+                fast_feedback::output<float_type> output{&cells(0,0), &cells(0,1), &cells(0,2), scores.data(), idx.cpers.max_output_cells};
+                idx.index(input, output, crt);
+            }
+
+            // spot access: spot i
+            inline float_type& spotX (unsigned i=0u) noexcept
+            { return coords(3u * idx.cpers.max_input_cells + i, 0u); }
+
+            inline const float_type& spotX (unsigned i) const noexcept
+            { return &coords(3u * idx.cpers.max_input_cells + i, 0u); }
+
+            inline float_type& spotY (unsigned i=0u) noexcept
+            { return coords(3u * idx.cpers.max_input_cells + i, 1u); }
+
+            inline const float_type& spotY (unsigned i=0u) const noexcept
+            { return coords(3u * idx.cpers.max_input_cells + i, 1u); }
+
+            inline float_type& spotZ (unsigned i=0u) noexcept
+            { return coords(3u * idx.cpers.max_input_cells + i, 2u); }
+
+            inline const float_type& spotZ (unsigned i=0u) const noexcept
+            { return coords(3u * idx.cpers.max_input_cells + i, 2u); }
+
+            inline auto spotM ()
+            { return coords.bottomRows(coords.rows() - 3u * idx.cpers.max_input_cells); }
+
+            // input cell access: cell i, vector j
+            inline float_type& iCellX (unsigned i=0u, unsigned j=0u) noexcept
+            { return coords(3u * i + j, 0u); }
+
+            inline const float_type& iCellX (unsigned i=0u, unsigned j=0u) const noexcept
+            { return coords(3u * i + j, 0u); }
+
+            inline float_type& iCellY (unsigned i=0u, unsigned j=0u) noexcept
+            { return coords(3u * i + j, 1u); }
+
+            inline const float_type& iCellY (unsigned i=0u, unsigned j=0u) const noexcept
+            { return coords(3u * i + j, 1u); }
+
+            inline float_type& iCellZ (unsigned i=0u, unsigned j=0u) noexcept
+            { return coords(3u * i + j, 2u); }
+
+            inline const float_type& iCellZ (unsigned i=0u, unsigned j=0u) const noexcept
+            { return coords(3u * i + j, 2u); }
+
+            inline auto iCellM ()
+            { return coords.topRows(3u * idx.cpers.max_input_cells); }
+
+            // output cell access: cell i, vector j
+            inline const float_type& oCellX (unsigned i=0u, unsigned j=0u) const noexcept
+            { return cells(3u * i + j, 0u); }
+
+            inline const float_type& oCellY (unsigned i=0u, unsigned j=0u) const noexcept
+            { return cells(3u * i + j, 1u); }
+
+            inline const float_type& oCellZ (unsigned i=0u, unsigned j=0u) const noexcept
+            { return cells(3u * i + j, 2u); }
+
+            inline auto& oCellM () noexcept
+            { return cells; }
+
+            // output cell score access: cell i
+            inline const float_type& oScore (unsigned i=0u) const noexcept
+            { return scores(i); }
+
+            inline auto& oScoreV () noexcept
+            { return scores; }
+
+            // runtime configuration access
+            inline void length_threshold (float_type lt)
+            {
+                if (lt < float_type{.0f})
+                    throw FF_EXCEPTION("negative length threshold");
+                crt.length_threshold = lt;
+            }
+
+            inline float_type length_threshold () const noexcept
+            { return crt.length_threshold; }
+
+            inline void num_sample_points (unsigned nsp)
+            {
+                unsigned tmp = crt.num_sample_points;
+                crt.num_sample_points = nsp;
+                try {
+                    check_config();
+                } catch (...) {
+                    crt.num_sample_points = tmp;
+                    throw;
+                }
+            }
+
+            inline unsigned num_sample_points () const noexcept
+            { return crt.num_sample_points; }
+
+            // persistent configuration access
+            // - to change the persistent config, create another indexer instance
+            inline unsigned max_output_cells () const noexcept
+            { return idx.cpers.max_output_cells; }
+
+            inline unsigned max_input_cells () const noexcept
+            { return idx.cpers.max_input_cells; }
+
+            inline unsigned max_spots () const noexcept
+            { return idx.cpers.max_spots; }
+
+            inline unsigned num_candidate_vectors () const noexcept
+            { return idx.cpers.num_candidate_vectors; }
+
+        }; // indexer
+
+        // least squares refinement indexer extra config
+        template <typename float_type=float>
+        struct config_lsq final {
+            float_type fit_threshold=.1;    // fit unit cells to spots closer than this to approximated Miller indices
+            float_type score_threshold=.2;  // calculate score as percentage of spots closer than this to approximated Miller indices
+        };
+
+        // least squares refinement indexer
+        template <typename float_type=float>
+        class indexer_lsq : public indexer<float_type> {
+            config_lsq<float_type> clsq;
+          public:
+            indexer_lsq (const fast_feedback::config_persistent<float_type>& cp,
+                         const fast_feedback::config_runtime<float_type>& cr,
+                         const config_lsq<float_type>& c)
+                : indexer<float_type>{cp, cr}, clsq{c}
+            {}
+
+            inline indexer_lsq (indexer_lsq&&) = default;
+            inline indexer_lsq& operator= (indexer_lsq&&) = default;
+            inline ~indexer_lsq () override = default;
+
+            indexer_lsq () = delete;
+            indexer_lsq (const indexer_lsq&) = delete;
+            indexer_lsq& operator= (const indexer_lsq&) = delete;
+
+            // refine output
+            void refine ()
+            {
+                using namespace std;
+                using namespace Eigen;
+                auto& coords = indexer<float_type>::coords;
+                auto& cells = indexer<float_type>::cells;
+                auto& scores = indexer<float_type>::scores;
+                auto& cpers = indexer<float_type>::idx.cpers;
+                MatrixX3f spots = coords.bottomRows(coords.rows() - 3u * cpers.max_input_cells);
+                for (unsigned j=0u; j<cpers.max_output_cells; j++) {
+                    Matrix3f cell = cells.block(3u * j, 0u, 3u, 3u).inverse();
+                    MatrixX3f temp = spots * cell;          // coordinates in system <cell>
+                    MatrixX3f miller = round(temp.array());
+                    temp -= miller;
+                    Vector<bool, Dynamic> thresh(temp.cols());
+                    thresh = temp.array().abs().rowwise().maxCoeff() < clsq.fit_threshold;
+                    Matrix<bool, Dynamic, 3u> sel(thresh.size(), 3u);
+                    sel.colwise() = thresh;
+                    FullPivHouseholderQR<MatrixX3f> qr(sel.select(miller, .0f));
+                    cell = qr.solve(sel.select(spots, .0f));
+                    cells.block(3u * j, 0u, 3u, 3u) = cell;
+                    temp = spots * cell.inverse();
+                    temp -= miller;
+                    thresh = temp.array().abs().rowwise().maxCoeff() < clsq.score_threshold;
+                    scores(j) = static_cast<float_type>(reduce(begin(thresh), end(thresh), 0u, plus<unsigned>{}))
+                                / static_cast<float_type>(spots.rows());
+                }
+            }
+
+            // refined indexing
+            void index (unsigned n_input_cells, unsigned n_spots) override
+            {
+                indexer<float_type>::index(n_input_cells, n_spots);
+                refine();
+            }
+
+            // lsq configuration access
+            inline void fit_threshold (float_type ft) noexcept
+            { clsq.fit_threshold = ft; }
+            
+            inline float_type fit_threshold () const noexcept
+            { return clsq.fit_threshold; }
+
+            inline void score_threshold (float_type st) noexcept
+            { clsq.score_threshold = st; }
+
+            inline float_type score_threshold () const noexcept
+            { return clsq.score_threshold; }
+
+        }; // indexer_lsq
+            
+    } // namespace refine
+} // namespace fast_feedback
+
+#endif // INDEXER_REFINE_H
--- tests/src/refined_simple_data_indexer.cpp
+++ tests/src/refined_simple_data_indexer.cpp
@@ -35,7 +35,7 @@ Author: hans-christian.stadler@psi.ch
 #include <Eigen/Dense>
 #include <Eigen/LU>
 #include "simple_data.h"
-#include "indexer.h"
+#include "refine.h"
 
 namespace {
 
@@ -102,92 +102,68 @@ int main (int argc, char *argv[])
             }
         }
 
-        float lsq_threshold = .2f;
+        fast_feedback::refine::config_lsq clsq{};   // default lsq refinement config
         {
-            std::istringstream iss(argv[6]);
-            iss >> lsq_threshold;
-            if (! iss)
-                throw std::runtime_error("unable to parse fifth argument: threshold for spot selection in least squares fitting");
-            std::cout << "lsq_threshold=" << lsq_threshold << '\n';
-            if ((lsq_threshold <= .0f) || (lsq_threshold > .5f))
-                throw std::runtime_error("lsq_threshold must be in range (0..0.5]");
+            {
+                std::istringstream iss(argv[6]);
+                iss >> clsq.fit_threshold;
+                if (! iss)
+                    throw std::runtime_error("unable to parse fifth argument: threshold for spot selection in least squares fitting");
+                std::cout << "lsq_threshold=" << clsq.fit_threshold << '\n';
+                if ((clsq.fit_threshold <= .0f) || (clsq.fit_threshold > .5f))
+                    throw std::runtime_error("lsq_threshold must be in range (0..0.5]");
+            }
+            {
+                std::istringstream iss(argv[7]);
+                iss >> clsq.score_threshold;
+                if (! iss)
+                    throw std::runtime_error("unable to parse sixth argument: threshold for spot selection in score calculation");
+                std::cout << "score_threshold=" << clsq.score_threshold << '\n';
+                if ((clsq.score_threshold <= .0f) || (clsq.score_threshold > .5f))
+                    throw std::runtime_error("score_threshold must be in range (0..0.5]");
+            }
         }
 
-        float score_threshold = .1f;
-        {
-            std::istringstream iss(argv[7]);
-            iss >> score_threshold;
-            if (! iss)
-                throw std::runtime_error("unable to parse sixth argument: threshold for spot selection in score calculation");
-            std::cout << "score_threshold=" << score_threshold << '\n';
-            if ((score_threshold <= .0f) || (score_threshold > .5f))
-                throw std::runtime_error("score_threshold must be in range (0..0.5]");
-        }
-
-        SimpleData<float, raise> data(argv[1]);         // read simple data file
-
-        Eigen::MatrixX3f coords(data.spots.size() + 3, 3); // coordinate container
-        unsigned i=0;
-        for (const auto& coord : data.unit_cell) {      // copy cell coordinates
-            coords(i, 0) = coord.x;
-            coords(i, 1) = coord.y;
-            coords(i, 2) = coord.z;
-            i++;
-        }
-        std::cout << "input:\n" << coords.block(0, 0, 3, 3) << '\n';
-        for (const auto& coord : data.spots) {          // copy spot coordinates
-            coords(i, 0) = coord.x;
-            coords(i, 1) = coord.y;
-            coords(i, 2) = coord.z;
-            i++;            
-        }
+        SimpleData<float, raise> data{argv[1]};         // read simple data file
 
         auto t0 = clock::now();
 
-        Eigen::MatrixX3f cells(3 * cpers.max_output_cells, 3);  // output cells coordinate container
-        Eigen::MatrixX2f scores(cpers.max_output_cells, 2);     // output cell scores container
-        fast_feedback::indexer indexer{cpers};          // indexer object
+        fast_feedback::refine::indexer_lsq indexer{cpers, crt, clsq};
 
-        fast_feedback::memory_pin pin_coords{coords};   // pin input coordinate container
-        fast_feedback::memory_pin pin_cells{cells};     // pin output cells coordinate container
-        fast_feedback::memory_pin pin_scores(scores);   // pin output cell scores container
-        fast_feedback::memory_pin pin_crt{fast_feedback::memory_pin::on(crt)}; // pin runtime config memory
-
-        fast_feedback::input<float> in{&coords(0,0), &coords(0,1), &coords(0,2), 1u, i-3u};                             // create indexer input object
-        fast_feedback::output<float> out{&cells(0,0), &cells(0,1), &cells(0,2), scores.data(), cpers.max_output_cells}; // create indexer output object
+        unsigned i=0u;
+        for (const auto& coord : data.unit_cell) {      // copy cell coordinates
+            indexer.iCellX(0, i) = coord.x;
+            indexer.iCellY(0, i) = coord.y;
+            indexer.iCellZ(0, i) = coord.z;
+            i++;
+        }
+        if (i != 3u)
+            throw std::logic_error("simple data contains more than one input cell");
+        std::cout << "input:\n" << indexer.iCellM() << '\n';
+        i=0;
+        for (const auto& coord : data.spots) {          // copy spot coordinates
+            indexer.spotX(i) = coord.x;
+            indexer.spotY(i) = coord.y;
+            indexer.spotZ(i) = coord.z;
+            if (++i == indexer.max_spots())
+                break;
+        }
 
         auto t1 = clock::now();
-        indexer.index(in, out, crt);                    // run indexer
+        static_cast<fast_feedback::refine::indexer<>&>(indexer).index(1u, i); // run unrefined indexing
         auto t2 = clock::now();
 
-        {                                               // refine cells
-            using namespace Eigen;
-            MatrixX3f spots = coords.bottomRows(coords.rows() - 3);
-            for (unsigned j=0u; j<out.n_cells; j++) {
-                Matrix3f cell = cells.block(3 * j, 0, 3, 3).inverse();
-                MatrixX3f temp = spots * cell;          // coordinates in system <cell>
-                MatrixX3f miller = round(temp.array());
-                temp -= miller;
-                Vector<bool, Dynamic> thresh(temp.cols());
-                thresh = temp.array().abs().rowwise().maxCoeff() < lsq_threshold;
-                Matrix<bool, Dynamic, 3> sel(thresh.size(), 3);
-                sel.colwise() = thresh;
-                FullPivHouseholderQR<Eigen::MatrixX3f> qr(sel.select(miller, .0f));
-                cell = qr.solve(sel.select(spots, .0f));
-                cells.block(3 * j, 0, 3, 3) = cell;
-                temp = spots * cell.inverse();
-                temp -= miller;
-                thresh = temp.array().abs().rowwise().maxCoeff() < score_threshold;
-                scores(j, 1) = (float)std::reduce(std::begin(thresh), std::end(thresh), 0u, [](unsigned a, unsigned b)->unsigned {return a + b;}) / (float)spots.rows();
-                scores(j, 0) /= -(3.f * spots.rows());
-            }
-        }
+        Eigen::VectorXf score_orig = indexer.oScoreV() / static_cast<float>(3u * i);
+        indexer.refine();                               // refine cells
 
         auto t3 = clock::now();
 
         std::cout << "output:\n";
-        for (unsigned j=0u; j<out.n_cells; j++)
-            std::cout << cells.block(3 * j, 0, 3, 3) << "\n\n"; // refined output cells
+        for (unsigned j=0u; j<cpers.max_output_cells; j++)
+            std::cout << indexer.oCellM().block(3u * j, 0u, 3u, 3u) << "\n\n"; // refined output cells
+        Eigen::MatrixX2f scores(score_orig.size(), 2);
+        scores.col(0) = score_orig;
+        scores.col(1) = indexer.oScoreV();
         std::cout << "scores:\n" << scores << '\n';             // scores
         std::cout << "timings:\n"
                   << "prep    " << duration{t1 - t0}.count() << "s\n"
