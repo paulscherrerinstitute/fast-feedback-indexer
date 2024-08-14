ce7d802741831d5b701fc67b20fdc90fe1fe94de
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2024-08-14 18:54:56 +0200

    Add pure components listing

--- set-version.sh
+++ set-version.sh
@@ -1,5 +1,5 @@
 function usage() {
-    echo "usage: $0 component [version]"
+    echo "usage: $0 [component [version] | components] "
     echo
     echo "Show or replace version strings with new version"
     exit -1
@@ -7,11 +7,15 @@ function usage() {
 
 COMPONENTS=($(find . -mindepth 2 -name set-version.sh))
 
-function unknown_component() {
-    echo "unknown component - use one of"
+function list_components() {
     for SCRIPT in "${COMPONENTS[@]}"; do
         echo "$SCRIPT"
     done
+}
+
+function unknown_component() {
+    echo "unknown component - use one of"
+    list_components
     exit -1
 }
 
@@ -24,14 +28,19 @@ if (($# == 0)); then
     done
 elif (($# > 2)); then
     usage
+elif [ "$1" == "components" ]; then
+    list_components
 else
-    SCRIPT="unknown_component"
+    SCRIPT=""
     for COMPONENT in "${COMPONENTS[@]}"; do
         if [ "$1" == "$COMPONENT" ]; then
             SCRIPT="$1"
             break
         fi
     done
+    if [ -z "$SCRIPT" ]; then
+        unknown_component
+    fi
     pushd $(dirname "$SCRIPT") &> /dev/null
     eval "./$(basename "$SCRIPT") $2"
     popd &> /dev/null
