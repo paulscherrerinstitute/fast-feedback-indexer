5b4621ab72f6f0280e2f47b5acba2cdf7a6cd135
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2024-08-14 17:53:26 +0200

    Fix set-version script

--- data/simple/reader/src/set-version.sh
+++ data/simple/reader/src/set-version.sh
@@ -9,8 +9,8 @@ if (($# == 0)); then
     grep VERSION meson.build
 elif (($# == 1)); then
     echo "replace version strings by $1"
-    sed -i 's/VERSION\(.*\)\w\.\w\.\w/VERSION\1'"$1"'/' CMakeLists.txt
-    sed -i 's/VERSION\(.*\)\w\.\w\.\w/VERSION\1'"$1"'/' meson.build
+    sed -i 's/VERSION\(.*\)\w\+\.\w\+\.\w\+/VERSION\1'"$1"'/' CMakeLists.txt
+    sed -i 's/VERSION\(.*\)\w\+\.\w\+\.\w\+/VERSION\1'"$1"'/' meson.build
 else
     usage
 fi
