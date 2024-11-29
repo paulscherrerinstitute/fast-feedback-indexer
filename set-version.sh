ba44288466cebf82401d001d28a620ece9ebea8c
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2024-11-29 11:30:01 +0100

    Bugfix: avoid showing minimum meson version

--- set-version.sh
+++ set-version.sh
@@ -6,11 +6,11 @@ function usage() {
 }
 
 if (($# == 0)); then
-    grep -Hn version meson.build
+    grep -Hn "\ version" meson.build
     grep -Hn "\ VERSION" CMakeLists.txt
 elif (($# == 1)); then
     echo "replace version strings by $1"
-    sed -i 's/version\(.*\)\w\+\.\w\+\.\w\+/version\1'"$1"'/' meson.build
+    sed -i 's/ version\(.*\)\w\+\.\w\+\.\w\+/ version\1'"$1"'/' meson.build
     sed -i 's/ VERSION\(.*\)\w\+\.\w\+\.\w\+/ VERSION\1'"$1"'/' CMakeLists.txt
 else
     usage
