77b0dbf10460699d4211c85675f0554d5e408cee
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2024-12-17 11:47:54 +0100

    Change version info to <project version>-<abrev git commit>, or just <project version> if not compiled from a git repo

--- indexer/src/GenerateVersionH.cmake
+++ indexer/src/GenerateVersionH.cmake
@@ -2,7 +2,7 @@ find_package(Git)
 
 if(GIT_EXECUTABLE)
     execute_process(
-        COMMAND ${GIT_EXECUTABLE} log --pretty=format:"%H: %aD" -1
+        COMMAND ${GIT_EXECUTABLE} log --pretty=format:%h -1
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
         OUTPUT_VARIABLE GIT_VERSION_TXT
         RESULT_VARIABLE ERROR_CODE
@@ -13,8 +13,7 @@ endif()
 if(GIT_VERSION_TXT STREQUAL "")
   set(VERSION_TXT "\"${VRS}\"")
 else()
-  string(REGEX REPLACE "^\"(.*)\"$" "\\1" GIT_VERSION_TXT "${GIT_VERSION_TXT}")
-  set(VERSION_TXT "\"${VRS}, ${GIT_VERSION_TXT}\"")
+  set(VERSION_TXT "\"${VRS}-${GIT_VERSION_TXT}\"")
 endif()
 
 message("Version detection in directory ${CMAKE_SOURCE_DIR} found: ${VERSION_TXT}")
